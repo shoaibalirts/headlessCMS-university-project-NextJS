9c60f6fb358086abffbe44b21e9da0b53636bc48
shoaibalirts <shoaibali.rts@gmail.com>
2025-02-02 12:26:25 +0100

    Added Categories in WP Dashboard and it will be reflected on my frontend

--- src/app/student-noticeboard/page.js
+++ src/app/student-noticeboard/page.js
@@ -1,11 +1,58 @@
-import { getStudentNoticeBoard } from "@/lib/univeristyapi";
-export default async function StudentNoticeBoardPage(){
-    const studentNoticeBoard = await getStudentNoticeBoard();
-    console.log(studentNoticeBoard);
-    
-    return (
-        <div>
-            <p>{studentNoticeBoard[0].content.rendered.replace(/<\/?[^>]+(>|$)/g, "")}</p>
-        </div>
-    );
+import he from "he";
+import {
+  getStudentNoticeBoard,
+  getStudentNoticeBoardCategoriesIds,
+  getStudentNoticeBoardCategoryPosts,
+} from "@/lib/univeristyapi";
+export default async function StudentNoticeBoardPage() {
+  const studentNoticeBoard = await getStudentNoticeBoard(); // only for page description or content
+  //   console.log(studentNoticeBoard);
+  // -------------------
+  // Get All Categories available in wordpress
+  const categories = await getStudentNoticeBoardCategoriesIds();
+  console.log("categories", categories);
+  // Get Posts for the Category of student-noticeboard via array filter method
+  const studentNoticeBoardCategories = categories.filter(
+    (category) => category.slug === "student-noticeboard"
+  );
+  console.log("studentNoticeBoardCategories", studentNoticeBoardCategories); // array of objects and each object has id which is a post id
+
+  // Each Category has id
+  // Sending the id of stdNoticeboardCategory to the API in order to get all posts array related to this category
+  const studentNotices = await getStudentNoticeBoardCategoryPosts(
+    studentNoticeBoardCategories[0].id
+  );
+  console.log("studentNotices", studentNotices);
+
+  return (
+    <>
+      <div className="p-4">
+        <p className="font-2xl">
+          {studentNoticeBoard[0].content.rendered.replace(
+            /<\/?[^>]+(>|$)/g,
+            ""
+          )}
+        </p>
+      </div>
+      {/* Posts Outer Container */}
+      <div className="mt-4 shadow-xl m-8">
+        {/* Card Container */}
+        {studentNotices.map((post) => {
+          return (
+            <div key={post.id} className="mt-4 border border-black p-2">
+              <h2 className="text-2xl font-bold">
+                {he.decode(post.title.rendered)}
+              </h2>
+              <p>{new Date(post.date_gmt).toLocaleDateString()}</p>
+              {/* <p>{post.content.rendered.replace(/<\/?[^>]+(>|$)/g, "")}</p> */}
+              <div
+                className="prose max-w-none" 
+                dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
--- src/lib/univeristyapi.js
+++ src/lib/univeristyapi.js
@@ -1,19 +1,43 @@
 export async function getPrograms() {
-    try {
-      const response = await fetch("http://localhost:8080/university/wordpress/wp-json/wp/v2/program");
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const response = await fetch(
+      "http://localhost:8080/university/wordpress/wp-json/wp/v2/program"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getStudentNoticeBoard(){
-    try {
-        const response = await fetch("http://localhost:8080/university/wordpress/wp-json/wp/v2/pages?slug=student-noticeboard");
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.log(error);
-      }
+export async function getStudentNoticeBoard() {
+  try {
+    const response = await fetch(
+      "http://localhost:8080/university/wordpress/wp-json/wp/v2/pages?slug=student-noticeboard"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getStudentNoticeBoardCategoriesIds() {
+  try {
+    
+    const response = await fetch("http://localhost:8080/university/wordpress/wp-json/wp/v2/categories?name=student-noticeboard");
+    const data = await response.json();
+    // console.log(data);
+    // `boligdetails/${home.id}`
+    return data;
+  } catch (error) {}
+}
+
+export async function getStudentNoticeBoardCategoryPosts(CATEGORY_ID) {
+  try {
+    
+    const response = await fetch(`http://localhost:8080/university/wordpress/wp-json/wp/v2/posts?categories=${CATEGORY_ID}`);
+    const data = await response.json();
+    
+    return data;
+  } catch (error) {}
 }
